54ef7fbea82c285a206a88654e01ec7ae2554917
euris-maran <daniel.maran@euris.it>
2017-10-03 13:14:20 +0200

    First issue

---
+++ Corso2017/CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First try");
+
+            Console.ReadLine();
+        }
+    }
+}
