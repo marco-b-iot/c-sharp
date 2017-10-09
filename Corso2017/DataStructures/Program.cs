1a518107b916f2a03de98c1b7abf27df36d0cf22
euris-maran <daniel.maran@euris.it>
2017-10-09 16:05:51 +0200

    Enum Array

---
+++ Corso2017/DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Length: " + numbers.Length);
+            Console.WriteLine("numbers[0] = " + numbers[0]);
+            numbers[2] = 5;
+            Console.WriteLine("numbers[2] = " + numbers[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
