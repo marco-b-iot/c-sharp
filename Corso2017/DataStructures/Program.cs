002914eed3128aa754f4813ae665f7552989ff5f
euris-maran <daniel.maran@euris.it>
2017-10-10 10:57:01 +0200

    Verifica input con int.TryParse

--- Corso2017/DataStructures/Program.cs
+++ Corso2017/DataStructures/Program.cs
@@ -5,6 +5,10 @@ namespace DataStructures
 {
     class Program
     {
+        private const string END_SUM = "=";
+
+        enum InputResult { Number, Equals, Error }
+        
         static void Main(string[] args)
         {
             ArrayList addends = new ArrayList();
@@ -15,15 +19,20 @@ namespace DataStructures
             {
                 Console.WriteLine($"Inserisci l'addendo ('=' per effettuare la somma)");
                 string input = Console.ReadLine();
-                if (input == "=")
+                InputResult result = VerifyInput(input, out int number);
+
+                if (result == InputResult.Equals)
                 {
                     break;
                 }
+                else if (result == InputResult.Number)
+                {
+                    addends.Add(number);
+                    sum += number;
+                }
                 else
                 {
-                    int addend = int.Parse(input);
-                    addends.Add(addend);
-                    sum += addend;
+                    Console.WriteLine("Non hai inserito un numero intero");
                 }
             }
 
@@ -31,5 +40,29 @@ namespace DataStructures
 
             Console.ReadLine();
         }
+
+        private static InputResult VerifyInput(string input, out int number)
+        {
+            InputResult result = InputResult.Error;
+
+            if (input == END_SUM)
+            {
+                number = 0;
+                result = InputResult.Equals;
+            }
+            else if (int.TryParse(input, out number))
+            {
+                result = InputResult.Number;
+            }
+
+            return result;
+        }
+
+
+        //private static void VerifyInput(ref int sum, out int originalValue)
+        //{
+        //    originalValue = sum;
+        //    sum += 10;
+        //}
     }
 }
