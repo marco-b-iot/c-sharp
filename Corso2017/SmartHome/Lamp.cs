8460a048f8a2510ddeb6bf83e456302777c7cc83
euris-maran <daniel.maran@euris.it>
2017-10-12 12:18:01 +0200

    Instanze e stati

---
+++ Corso2017/SmartHome/Lamp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    public class Lamp
+    {
+        private bool _isOn;
+
+        public Lamp(string room /*= "N/A"*/)
+        {
+            //if (string.IsNullOrEmpty(room))
+            //    room = "N/A";
+
+            Room = room;
+        }
+
+        public string Room { get; private set; }
+        
+        //public string GetStatus()
+        //{
+        //    return IsOn ? "on" : "off";
+        //}
+
+        public string Status
+        {
+            get
+            {
+                return _isOn ? "on" : "off";
+            }
+        }
+
+        public bool TurnOn()
+        {
+            _isOn = true;
+
+            return _isOn;
+        }
+
+        public bool TurnOff()
+        {
+            _isOn = false;
+
+            return _isOn;
+        }
+
+        public bool TurnOn(int intensity)
+        {
+            _isOn = true;
+            //set intensity
+
+            return _isOn;
+        }
+    }
+}
---
+++ Corso2017/SmartHome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lamp salonLamp = new Lamp("Salon");
+            //salon.Room = "Salon";
+            //string status = salon.IsOn ? "on" : "off";
+
+            WriteStatus(salonLamp);
+            salonLamp.TurnOn();
+            WriteStatus(salonLamp);
+        
+            Lamp kitchenLamp = new Lamp("Kitchen");
+            WriteStatus(kitchenLamp);
+
+            Lamp kitchenBackup = kitchenLamp;
+            kitchenLamp = salonLamp;
+            kitchenLamp.TurnOff();
+
+            WriteStatus(salonLamp);
+
+            Console.ReadLine();
+        }
+
+        private static void WriteStatus(Lamp lamp)
+        {
+            Console.WriteLine($"{ lamp.Room } lamp is { lamp.Status }");
+        }
+    }
+}
